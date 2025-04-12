1a42acad3c1e3cf9840e1d3067d842f2159aaab4
Naveen-Dilsh <naveentype2001@gmail.com>
2025-04-12 15:45:27 +0530

    Add constant and common files

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,26 @@
+/// This class contains all the App Images in String formats.
+class APPImages {
+  // -- App Logos
+  static const String darkAppLogo = "assets/logos/Google.png";
+  static const String lightAppLogo = "assets/logos/Google.png";
+
+  // -- Social Logos
+  static const String google = "assets/logos/Google.png";
+  static const String facebook = "assets/logos/Facebook.png";
+
+  // -- OnBoarding Images
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/boy.gif";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/listen-to-17266_512.gif";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/super.gif";
+
+  // -- Animation Images
+  static const String productsIllustration = "assets/animations/boy.gif";
+  static const String productsSaleIllustration = "assets/animations/boy.gif";
+  static const String staticSuccessIllustration = "assets/animations/boy.gif";
+  static const String deliveredInPlaneIllustration = "assets/animations/boy.gif";
+  static const String deliveredEmailIllustration = "assets/animations/boy.gif";
+  static const String verifyIllustration = "assets/animations/boy.gif";
+
+  // --
+  static const String docerAnimation = "assets/logos/Facebook.png";
+}
