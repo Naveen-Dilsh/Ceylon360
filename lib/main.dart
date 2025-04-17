e47bebb1742bbe1f60312bbf96df8320cdcbca78
Naveen-Dilsh <naveentype2001@gmail.com>
2025-04-17 20:52:32 +0530

    Setup the firebase Console

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:ceyloan_360/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
   //TODO: Add widgets Binding
@@ -14,6 +17,9 @@ Future<void> main() async {
   //TODO: Await Native Splash
 
   //Initialize Firebase && Authentication Repository
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
 
   //TODO: Initialize Authentication
 
